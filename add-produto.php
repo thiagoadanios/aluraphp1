a5165d850228cf6dcc6d46c3f58603f95622a90a
thiagoadanios <thiagoadaniosc@gmail.com>
2017-04-17 03:03:27 -0300

    17/abr

--- add-produto.php
+++ add-produto.php
@@ -1,29 +1,23 @@
-<?php 
-include("header.php");			
+<?php
+include("header.php");
 
-	//		if (empty($_GET["nome"]) or empty($_GET["preco"])) {
-	//			$preco = "null";
-	//			$nome = "null";
-	//		} else {
-				$nome = $_GET["nome"];
-				$preco = $_GET["preco"];
-	//		}
+$nome = $_GET["nome"];
+$preco = $_GET["preco"];
 $query = "INSERT INTO PRODUTOS (NOME, PRECO) VALUES ('{$nome}', {$preco});";
 $conexao = mysqli_connect("localhost", "root","", "loja");
-
-if (mysqli_query($conexao, $query)){
-
+	if (mysqli_query($conexao, $query)){
 ?>
 
-<p class="alert-success"> Produto
-    <?=$nome;?>,
-        <?=$preco;?> Adicionado com Sucesso
-</p>
-<?php } else {
-?>
-<p class="alert-danger"> Produto não foi adcionado</p>
+    <p class="alert-success"> Produto
+        <?=$nome;?>, <?=$preco;?> Adicionado com Sucesso
+    </p>
+    <?php } else { ?>
+    <p class="alert-danger"> Produto não foi adcionado</p>
+
 <?php
 }
-mysqli_close($conexao); // Não Obrigatorio 
 
-include("footer.php"); ?>
+mysqli_close($conexao); // Não Obrigatorio
+
+include("footer.php"); 
+?>
