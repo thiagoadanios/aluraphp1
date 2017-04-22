24ed441a1d95605123254245c32baf46f4d16afb
thiagoadanios <thiagoadaniosc@gmail.com>
2017-04-22 15:20:58 -0300

    Alteração Funções

--- add-produto.php
+++ add-produto.php
@@ -1,26 +1,36 @@
+<?php  include("header.php");  ?>
+
 <?php
-//ini_set('display_errors', 1);
-include("header.php");
+$conexao = mysqli_connect("localhost", "root","root", "loja");
 $nome = isset($_GET['nome']) ? $_GET['nome'] : 'valor padrão';
-$preco = isset($_GET['preco']) ? $_POST['preco'] : 'valor padrão';
-echo "{$nome} , {$preco}";
-$query = "INSERT INTO PRODUTOS (NOME, PRECO) VALUES ('{$nome}', {$preco});";
+$preco = (isset($_GET['preco'])  && is_numeric($_GET['preco'])) ? $_GET['preco']  : 'valor padrão';
 
-$conexao = mysqli_connect("localhost", "root","root", "mysql");
+echo $preco;
 
-	if (mysqli_query($conexao, $query)){
+function InsereProduto($conexao, $nome, $preco)  : int {
+	$query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}', {$preco});";
+	return mysqli_query($conexao, $query);
+}
+
+try {
+//$mysqli = new mysqi
+
+	if (InsereProduto($conexao, $nome, $preco)){
 
 ?>
-
-<p class="alert-success"> Produto
-	<?=$nome;?>, <?=$preco;?> Adicionado com Sucesso
-</p>
+	
+	<p class="text-success"> Produto
+		<?=$nome;?>, <?=$preco;?> Adicionado com Sucesso
+	</p>
 <?php } else { ?>
-<p class="alert-danger"> <?=$nome?> Produto não foi adcionado</p>
+
+	<p class="text-danger"> <?=$nome?> Produto não foi adcionado</p>
 
 <?php
 }
 
 mysqli_close($conexao); // Não Obrigatorio
-include("footer.php");
-?>
+} catch (mysqli_sql_exception $e) {
+	echo "deu merda";
+}?>
+<?php  include("footer.php");  ?>
--- header.php
+++ header.php
@@ -1,13 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-    <head>
-        <meta charset="UTF-8">
-        <title> Minha Loja</title>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="css/loja.css">
-    </head>
-
-    <body>
-        <div class="container">
-            <div class="principal">
+    	<head>
+        		<meta charset="UTF-8">
+        		<title> Minha Loja</title>
+        		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        		<link rel="stylesheet" type="text/css" href="css/loja.css">
+    	</head>
+    
+    	<body>
+		<header class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">Minha Loja</a>
+			</div>
+			<nav>
+				<ul class="nav navbar-nav">
+					<li><a href="formulario.php">Adicionar Produto</a></li>
+					<li><a href="formulario.php">Adicionar Produto</a></li>
+					<li><a href="formulario.php">Adicionar Produto</a></li>
+				</ul>
+			</nav>
+		</header>
+        		<div class="container">
+            	<div class="principal">
